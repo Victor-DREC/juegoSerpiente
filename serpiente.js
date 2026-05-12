3bcb449541119aa08fe750457ad8f1c8f96b5b68
Victor-DREC <victorpilachanga12@gmail.com>
2026-05-12 11:43:01 -0500

    dibujo tablero FIN PARTE 2

--- serpiente.js
+++ serpiente.js
@@ -1,7 +1,11 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+
+const tamañoCelda = 25; 
+const filas = canvas.height / tamañoCelda;
+const columnas = canvas.width / tamañoCelda;
 
 
     
@@ -13,12 +17,34 @@
     // FUNCIONES DE DIBUJO
     // =========================
 
+
+function dibujarTablero() {
+    ctx.strokeStyle = "rgba(255, 255, 255, 0.18)"; 
+    ctx.lineWidth = 1;
+
+    for (let x = 0; x <= canvas.width; x += tamañoCelda) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.stroke();
+    }
+
+    for (let y = 0; y <= canvas.height; y += tamañoCelda) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvas.width, y);
+        ctx.stroke();
+    }
+}
+
+
     function limpiarCanvas() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
     function dibujarTodo() {
       limpiarCanvas();
+      dibujarTablero();
     }
 
 
